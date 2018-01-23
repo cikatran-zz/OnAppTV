6345dae1d148164a1008e1e3a7377dedab1e83b3
CiKa <cao.tranchi@spout360.com>
2018-01-23 17:06:27 +0700

    Add Server Host config function

--- android/app/src/main/java/com/onapptv/MainActivity.java
+++ android/app/src/main/java/com/onapptv/MainActivity.java
@@ -1,13 +1,35 @@
 package com.onapptv;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private static final String PREFS_DEBUG_SERVER_HOST_KEY = "debug_http_host";
+    private static String HOST_SERVER = "192.168.10.112:8081";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+        mEditor.apply();
+        mEditor.putString(PREFS_DEBUG_SERVER_HOST_KEY, HOST_SERVER);
+        mEditor.commit();
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
     @Override
     protected String getMainComponentName() {
         return "OnAppTV";
