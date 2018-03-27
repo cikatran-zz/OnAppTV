e0561f5b2ca23a61b0d167e95295a8abeba40c02
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-03-27 15:38:50 +0700

    Fix ignoring android src

---
+++ android/app/src/main/java/com/onapptv/ReactBrightcovePlayerViewManager.java
@@ -0,0 +1,39 @@
+package com.onapptv;
+
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.onapptv.custombrightcoveplayer.CustomBrightcovePlayer;
+
+/**
+ * Created by oldmen on 3/7/18.
+ */
+
+public class ReactBrightcovePlayerViewManager extends SimpleViewManager<CustomBrightcovePlayer> {
+    public static final String REACT_CLASS = "RNTBrightcovePlayer";
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    protected CustomBrightcovePlayer createViewInstance(ThemedReactContext reactContext) {
+        return new CustomBrightcovePlayer(reactContext);
+    }
+
+    @ReactProp(name = "videoId")
+    public void setVideoId(CustomBrightcovePlayer view, String videoId) {
+        view.setVideoKey(videoId);
+    }
+
+    @ReactProp(name = "accountId")
+    public void setAccountId(CustomBrightcovePlayer view, String accountId) {
+        view.setAccountId(accountId);
+    }
+
+    @ReactProp(name = "policyKey")
+    public void setPolicyKey(CustomBrightcovePlayer view, String policyKey) {
+        view.setPolicyKey(policyKey);
+    }
+}
