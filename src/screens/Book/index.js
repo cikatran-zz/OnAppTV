8d0465a7d67ad4627932347aeb92257cb4e08b5e
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-04-03 15:26:57 +0700

    [UPDATE] add call statement

--- src/screens/Book/index.js
+++ src/screens/Book/index.js
@@ -1,15 +1,15 @@
 import Book from './Book'
 import {connect} from "react-redux";
+import getBookList from '../../actions/getBookList'
 
 function mapStateToProps(state) {
   return {
-
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-
+    getList: () => dispatch(getBookList())
   }
 }
 
