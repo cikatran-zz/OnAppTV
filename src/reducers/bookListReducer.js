160be1a4279507a7f11601a8f5a882d0054cc4b9
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-04-03 15:26:30 +0700

    [ADD] Book List call

---
+++ src/reducers/bookListReducer.js
@@ -0,0 +1,35 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  data: null,
+  fetched: false,
+  isFetching: false,
+  error: false
+};
+
+export default function bookListReducer(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.FETCHING_BOOK_LIST:
+      return {
+        ...state,
+        data: null,
+        isFetching: true
+      };
+    case actionTypes.FETCH_BOOK_LIST_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        fetched: true,
+        data: action.data
+      };
+    case actionTypes.FETCH_BOOK_LIST_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        fetched: true,
+        errorMessage: action.errorMessage
+      };
+    default:
+      return state
+  }
+}
