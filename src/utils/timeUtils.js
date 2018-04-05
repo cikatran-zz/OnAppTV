9ce17220b1a97b46e175dac4ba03ac47ee6c398f
Phuc Huynh <phuchuynh.strong@gmail.com>
2018-04-05 10:57:51 +0700

    [UPDATE] timutil seconds

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -5,13 +5,15 @@ export const timeFormatter = (time) => {
     return ((hours < 10 ? '0' + hours : hours) + ":" + (minutes < 10 ? '0' + minutes : minutes))
 };
 
-export const secondFormatter = (seconds) => {
-    let minutes = Math.floor(seconds/ 60);
-    let hours = Math.floor(seconds/ 3600);
-    if (hours == 0) {
-        return (minutes < 10 ? '0' + minutes : minutes) + 'min';
-    } else {
-        return ((hours < 10 ? '0' + hours : hours) + "h" + (minutes < 10 ? '0' + minutes : minutes)) + 'min';
-    }
+export const secondFormatter = (duration) => {
+  let sec_num = parseInt(duration, 10); // don't forget the second param
+  let hours   = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return hours+':'+minutes+':'+seconds;
 };
 
