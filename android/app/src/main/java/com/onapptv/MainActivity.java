1bdb1a39eb91548cf0219a2317ed4f8630f0b57f
CiKa <cao.tranchi@gmail.com>
2018-04-19 10:43:34 +0700

    Remove Gesture Handler

--- android/app/src/main/java/com/onapptv/MainActivity.java
+++ android/app/src/main/java/com/onapptv/MainActivity.java
@@ -3,9 +3,6 @@ package com.onapptv;
 import android.content.SharedPreferences;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -28,16 +25,6 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
-    }
 
     @Override
     protected String getMainComponentName() {
