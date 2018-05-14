77863311e9fde93132367061d776997e4735aee8
phuchuynh <phucvn96>
2018-05-14 20:14:28 +0700

    Set scaled image source

--- src/utils/images.js
+++ src/utils/images.js
@@ -5,10 +5,14 @@ export function getImageFromArray(images, firstImageName, secondImageName) {
     let image = null;
     images.forEach((item)=> {
         if (item.name === firstImageName) {
-            image = item.url;
+            let scaledImages = item.scaledImage
+            if (scaledImages != null)
+                image = scaledImages[scaledImages.length - 1].url;
         }
         if (image == null && item.name === secondImageName) {
-            image = item.url;
+            let scaledImages = item.scaledImage
+            if (scaledImages != null)
+                image = scaledImages[scaledImages.length - 1].url;
         }
     });
     image = (image == null) ? images[0].url : image;
