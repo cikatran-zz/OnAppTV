6c84c0fe6e6201fc8b583c793abd5907fd86ad79
Simon Pham <simon@hasbrain.com>
2018-07-20 17:39:37 +0700

    Update recordTimeshift method

--- ios/NativeComponent/ControlModal/TimeshiftHelper.swift
+++ ios/NativeComponent/ControlModal/TimeshiftHelper.swift
@@ -8,27 +8,42 @@
 
 import STBAPI
 
-let TIMESHIFT_FILE_NAME = "Timeshift"
+let TIMESHIFT_FILE_NAME = "timeshift"
 
 func recordTimeshift(lcn: Int32, startTime: Date, duration: Int32) {
-    stopRecordTimeshift()
-    deleteTimeshiftRecord()
-
-    let recordModel = RecordModel()
-    recordModel.recordMode = 1
-    recordModel.recordName = TIMESHIFT_FILE_NAME
-
-    recordModel.lCN = lcn
-    recordModel.startTime = startTime
-    recordModel.duration = duration
-    
-    print("RECORDING TIMESHIFT")
-    Api.shared().hIG_RecordPvrStart(
-        withRecordParameter: recordModel,
-        metaData: "",
-        callback: { (isSuccess, error) in
-            printError(isSuccess: isSuccess, error: error)
-    })
+    Api.shared().hIG_RecordPvrStop { (stopSuccess, stopError) in
+        print("Stop Record \(stopSuccess) \(stopError)")
+        Api.shared().hIG_DeletePvr(withRecordName: TIMESHIFT_FILE_NAME, callback: { (deleteSuccess, deleteError) in
+            print("Delete record \(deleteSuccess) \(deleteError)")
+            Api.shared()?.hIG_GetUSBDisks({ (usbDiskArray) in
+                if usbDiskArray?.count != 0 {
+                    let disk = usbDiskArray?.lastObject as! DiskModel;
+                    let partiton = disk.partitionArr.lastObject as! PartitonModel;
+                    Api.shared().hIG_SetPvrPath(withPartition: partiton, callback: { (setPathSuccess, setPathError) in
+                        if setPathSuccess == true {
+                            let record = RecordModel()
+                            record.lCN = lcn
+                            record.startTime = Date.init()
+                            record.duration = 0
+                            record.recordMode = 0
+                            record.recordName = TIMESHIFT_FILE_NAME
+                            Api.shared().hIG_RecordPvrStart(withRecordParameter: record, metaData: "", callback: { (recordSuccess, recordError) in
+                                print("Start record \(recordSuccess) \(recordError)")
+                                if (recordSuccess) {
+                                    // A delay is required for PlayPvrStart to work
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                        Api.shared().hIG_PlayPvrStart(withRecordName: TIMESHIFT_FILE_NAME, playPosition: 0) { (playSuccess, playError) in
+                                            print("Start play \(playSuccess) \(playError)")
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        })
+    }
 }
 
 func stopRecordTimeshift() {
