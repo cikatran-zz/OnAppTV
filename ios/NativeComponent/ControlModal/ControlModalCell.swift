f98a28729fd8b77dfa42005cdfeea6cf91a0d1d1
Simon Pham <simon@hasbrain.com>
2018-07-23 10:02:27 +0700

    Update shiftTo method

--- ios/NativeComponent/ControlModal/ControlModalCell.swift
+++ ios/NativeComponent/ControlModal/ControlModalCell.swift
@@ -204,8 +204,9 @@ extension ControlModalCell {
     }
     
     func shiftTo(_ currentTime: Double, callback:@escaping (Bool)-> Void) {
-        Api.shared().hIG_PlayPvrSetPosition(
-            withPosition: Int32((currentTime.isNaN || currentTime.isInfinite) ? 0 : currentTime),
+        Api.shared().hIG_PlayPvrStart(
+            withRecordName: TIMESHIFT_FILE_NAME,
+            playPosition: Int32((currentTime.isNaN || currentTime.isInfinite) ? 0 : currentTime),
             callback: { (isSuccess, error) in
                 if (!isSuccess) {
                     print(error ?? "")
