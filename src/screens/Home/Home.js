67bc374db72396cda5b0fe8daa69fcfc7c2c6a63
CiKa <cao.tranchi@gmail.com>
2018-07-24 11:35:10 +0700

    Change margin of notification text to 18px

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -872,11 +872,11 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 130,
         borderRadius: 10,
-        resizeMode: 'cover'
+        resizeMode: 'cover',
     },
     notificationTitle: {
         ...textDarkDefault,
-        marginVertical: 5
+        marginVertical: 18,
     },
     notificationSubTitle: {
         ...textLightDefault
@@ -948,7 +948,7 @@ const styles = StyleSheet.create({
         top: 0,
         left: 0,
         right: 0,
-        bottom: 50,
+        height: 130,
         zIndex: -1,
         borderWidth: 0.5,
         borderColor: colors.textGrey,
