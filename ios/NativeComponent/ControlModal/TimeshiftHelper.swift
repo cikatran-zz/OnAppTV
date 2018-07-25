76150dc1f209a149a04dd8d7692b33f00b9e1ee9
Simon Pham <simon@hasbrain.com>
2018-07-25 10:59:02 +0700

    Update TimeshiftHelper

--- ios/NativeComponent/ControlModal/TimeshiftHelper.swift
+++ ios/NativeComponent/ControlModal/TimeshiftHelper.swift
@@ -10,11 +10,9 @@ import STBAPI
 
 let TIMESHIFT_FILE_NAME = "timeshift"
 
-func recordTimeshift(lcn: Int32) {
+func recordTimeshift(lcn: Int32, callback:@escaping (Bool)-> Void) {
     Api.shared().hIG_RecordPvrStop { (stopSuccess, stopError) in
-        print("Stop Record \(stopSuccess) \(stopError)")
         Api.shared().hIG_DeletePvr(withRecordName: TIMESHIFT_FILE_NAME, callback: { (deleteSuccess, deleteError) in
-            print("Delete record \(deleteSuccess) \(deleteError)")
             Api.shared()?.hIG_GetUSBDisks({ (usbDiskArray) in
                 if usbDiskArray?.count != 0 {
                     let disk = usbDiskArray?.lastObject as! DiskModel;
@@ -28,15 +26,7 @@ func recordTimeshift(lcn: Int32) {
                             record.recordMode = 0
                             record.recordName = TIMESHIFT_FILE_NAME
                             Api.shared().hIG_RecordPvrStart(withRecordParameter: record, metaData: "", callback: { (recordSuccess, recordError) in
-                                print("Start record \(recordSuccess) \(recordError)")
-                                if (recordSuccess) {
-                                    // A delay is required for PlayPvrStart to work
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                        Api.shared().hIG_PlayPvrStart(withRecordName: TIMESHIFT_FILE_NAME, playPosition: 0) { (playSuccess, playError) in
-                                            print("Start play \(playSuccess) \(playError)")
-                                        }
-                                    }
-                                }
+                                callback(recordSuccess)
                             })
                         }
                     })
@@ -46,32 +36,26 @@ func recordTimeshift(lcn: Int32) {
     }
 }
 
-func stopRecordTimeshift() {
+func stopRecordTimeshift(callback:@escaping (Bool)-> Void) {
     print("STOP RECORDING")
     Api.shared().hIG_RecordPvrStop { (isSuccess, error) in
-        printError(isSuccess: isSuccess, error: error)
+        callback(isSuccess)
     }
 }
 
-func deleteTimeshiftRecord() {
+func deleteTimeshiftRecord(callback:@escaping (Bool)-> Void) {
     print("DELETE EXISTING TIMESHIFT RECORD")
     Api.shared().hIG_DeletePvr(withRecordName: TIMESHIFT_FILE_NAME) { (isSuccess, error) in
-        printError(isSuccess: isSuccess, error: error)
+        callback(isSuccess)
     }
 }
 
-func playTimeshift(playPosition: Int32) {
+func playTimeshift(playPosition: Int32, callback:@escaping (Bool)-> Void) {
     print("PLAYING TIMESHIFT at position: " + String(playPosition))
     Api.shared().hIG_PlayPvrStart(
         withRecordName: TIMESHIFT_FILE_NAME,
         playPosition: playPosition,
         callback: { (isSuccess, error) in
-            printError(isSuccess: isSuccess, error: error)
+            callback(isSuccess)
     })
-}
-
-func printError(isSuccess: Bool, error: String?) {
-    if (!isSuccess) {
-        print(error ?? "")
-    }
 }
